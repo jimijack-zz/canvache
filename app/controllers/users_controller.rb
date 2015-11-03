bebb5fb5e168e59fe86ac2c4c9e130271c1ab193
James Ewing <olearysound@gmail.com>
2015-11-02 17:10:50 -0800

    Added redirect params to user create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user.id)
       flash[:notice] = "You have successfully signed up!"
-      redirect_to root_path
     else
       render 'new'
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :galleries
+  validates :name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { in: 6..20}
+  validates :gallery_rep,
 end
