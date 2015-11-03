899c6ccd298132821cefabf84d2d84738bd07696
James Ewing <olearysound@gmail.com>
2015-11-02 18:17:03 -0800

    Added Bootstrap and helper methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,4 @@ class User < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { in: 6..20}
-  validates :gallery_rep,
 end
