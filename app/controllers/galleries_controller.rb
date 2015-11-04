8743f755b8a3a5ae056680087c76f8f5347a2946
James Ewing <olearysound@gmail.com>
2015-11-03 20:12:05 -0800

    forgot rake db:migrate

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -6,4 +6,6 @@ class GalleriesController < ApplicationController
 
   #users.gallery.create
 
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151104040220) do
+ActiveRecord::Schema.define(version: 20151104040831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,7 +40,10 @@ ActiveRecord::Schema.define(version: 20151104040220) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.string   "zip_code"
+    t.integer  "user_id"
   end
+
+  add_index "galleries", ["user_id"], name: "index_galleries_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -52,4 +55,5 @@ ActiveRecord::Schema.define(version: 20151104040220) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "galleries", "users"
 end
